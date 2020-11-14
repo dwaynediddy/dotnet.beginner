4af69ad5de0f78b187b76e8968697ccb908fc4e0
dwaynediddy <burrows.dwayne@icloud.com>
2020-11-14 21:42:29 +1000

    dotnet prac after docs tutorial

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//NAMESPACE
+namespace dotnet
+{
+// Main Class
+    class Program
+    {
+    //entry point method
+        static void Main(string[] args)
+        //set app variables
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Dwayne Burrows";
+
+            //color changer
+            Console.ForegroundColor = ConsoleColor.Green;
+            // write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            //RE - Set color scheme
+            Console.ResetColor();
+
+            //ask user name
+            Console.WriteLine("What is your name?");
+
+            //Get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game...", input);
+
+            
+
+        }
+    }
+}
