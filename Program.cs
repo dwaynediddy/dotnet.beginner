03611c0854fc1beffd01673cee7eeec8e82ab331
dwaynediddy <burrows.dwayne@icloud.com>
2020-11-14 22:15:51 +1000

    added wrong and correct messages for number although not random yet

--- Program.cs
+++ Program.cs
@@ -25,11 +25,47 @@ namespace dotnet
             Console.WriteLine("What is your name?");
 
             //Get user input
-            string input = Console.ReadLine();
+            string inputName = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}, lets play a game...", input);
+            Console.WriteLine("Hello {0}, lets play a game...", inputName);
 
-            
+            //set correct number of
+            int correctNumber = 7;
+
+            //guess variable
+            int guess = 0;
+
+            //ask user to guess Number
+            Console.WriteLine("Guess a Number between 1 and 10");
+
+            //while guess is not correct
+            while(guess != correctNumber) {
+                //getuser input
+                string input = Console.ReadLine();
+
+                //cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                //match guess to correct number and
+                if(guess != correctNumber) {
+                //change color
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                //write out app info to
+                Console.WriteLine("WRONG, again.");
+
+                Console.ResetColor();
+                }
+
+            }
+
+            //success message when numbers guess correct
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //tell user answer was guessed correctly
+            Console.WriteLine("That is CORRECT!");
+
+            Console.ResetColor();
 
         }
     }
